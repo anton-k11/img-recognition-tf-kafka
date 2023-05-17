eddcd1d30c98958c9269ff73a53ae2bced8fe045
Anton Krosnev <anton.krosnev@gmail.com>
2023-05-17 12:00:07 +0300

    Add Kafka Streaming component

---
+++ ml-img-recognition/src/main/java/img/recognition/ml/kafka/ImgJsonKafkaStream.java
@@ -0,0 +1,53 @@
+package img.recognition.ml.kafka;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor_={@Autowired})
+public class ImgJsonKafkaStream {
+
+    private final KafkaStreamConfig config;
+
+    @Autowired
+    public void kafkaStreams(StreamsBuilder streamsBuilder) {
+        String consumerTopic = config.getConsumerTopic();
+        String producerTopic = config.getProducerTopic();
+        log.info("Consuming from {} and producing to {} topics", consumerTopic, producerTopic);
+
+        KStream<String, byte[]> stream = streamsBuilder
+                .stream(consumerTopic, Consumed.with(Serdes.String(), Serdes.ByteArray()));
+
+        // Transform the image into JSON (replace with your own transformation code)
+        KStream<String, String> transformedStream = stream.mapValues(
+                value -> {
+                    log.info("Message with length {} consumed", value.length);
+                    // Replace this with your transformation logic
+                    String json = transformImageToJson(value);
+
+                    // Return the new key-value pair with preserved headers
+                    return json;
+                });
+
+        transformedStream.to(producerTopic);
+
+        // Print the transformed stream (optional)
+        //transformedStream.print(Printed.toSysOut());
+    }
+
+    // Replace this with your own image-to-JSON transformation logic
+    private String transformImageToJson(byte[] image) {
+        // TODO: Implement image-to-JSON transformation
+        return "{\"juice\":58, \"jam\": 42}";
+    }
+
+
+}
